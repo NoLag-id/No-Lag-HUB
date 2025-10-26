b929111cec78085fd7ef1d04326dd0f6af7a2821
deopzalol <akbarpratanegara@gmail.com>
2025-10-26 14:07:33 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,8 +1,8 @@
 repeat task.wait() until game:IsLoaded() and game:GetService("Players") and game:GetService("Players").LocalPlayer and game:GetService("Players").LocalPlayer.Character
-if game.placeId == 127742093697776 then
+--[[if game.placeId == 127742093697776 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f0652d5d8288eb119ed2704a08d1618f.lua"))()
     return
-end
+end]]
 local scripts = {
     [126884695634066] = "7a953911595e67e8494c3d3446b8be5b",
     [124977557560410] = "7a953911595e67e8494c3d3446b8be5b", 
